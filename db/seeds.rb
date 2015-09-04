254a90788121883052ccdaac4d13007f5da770cc
Henry Phan <htphan.dev@gmail.com>
2015-09-03 22:51:22 -0400

    Moved utility.rb to seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+users = HTTParty.get('http://jsonplaceholder.typicode.com/users')
+users.each do |user|
+  new_user = User.create(
+    name: user['name'], username: user['username'], 
+    email: user['email'], phone: user['phone'], 
+    website: user['website'])
+  user_address = Address.create( user_id: new_user.id,
+    street: user['address']['street'], suite: user['address']['suite'],
+    city: user['address']['city'], zipcode: user['address']['zipcode'])
+end
+
+albums = HTTParty.get('http://jsonplaceholder.typicode.com/albums')
+albums.each do |album|
+  new_album = Album.create(user_id: album['userId'], title: album['title'])
+  photos = HTTParty.get("http://jsonplaceholder.typicode.com/photos?albumId=#{new_album.id}")
+  photos.each do |photo|
+    new_photo = Photo.create(
+      album_id: photo['albumId'], title: photo['title'],
+      url: photo['url'], thumbnail_url: photo['thumbnailUrl']
+      )
+  end
+end
